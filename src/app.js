8bf82668daaddfabc51185d16557eeb3f4f846c1
cmlee <bal.dongdongdong@gmail.com>
2020-03-28 15:16:43 +0900

    init commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-// const foo = "";
+import axios from 'axios';
+import './app.css';
 
-console.log("hello");
+document.addEventListener('DOMContentLoaded', async () => {
+  const res = await axios.get('/api/users');
+
+  document.body.innerHTML = (res.data || [])
+    .map(user => `<div>${user.id}: ${user.name}</div>`)
+    .join('');
+});
+
+if (module.hot) {
+  console.log('핫 모듈 켜짐');
+
+  module.hot.accept('./result', () => {
+    console.log('result 모듈 변경 됨');
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,63 @@ const childProcess = require('child_process');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const apiMocker = require('connect-api-mocker');
+
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  mode: 'development',
+  externals: {
+    axios: 'axios'
+  },
+  mode,
   entry: {
-    main: './src/app.js',
+    main: './src/app.js'
   },
   output: {
     path: path.resolve('./dist'),
-    filename: '[name].js',
+    filename: '[name].js'
+  },
+  devServer: {
+    overlay: true,
+    stats: 'errors-only',
+    before: app => {
+      app.use(apiMocker('/api', './mocks/api'));
+    },
+    proxy: {
+      '/api': 'http://localhost:8081'
+    },
+    hot: true
+  },
+  optimization: {
+    minimizer:
+      mode === 'production'
+        ? [
+            new OptimizeCSSAssetsPlugin(),
+            new TerserWebpackPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true
+                }
+              }
+            })
+          ]
+        : []
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'url-loader',
         options: {
           name: '[name].[ext]?[hash]',
-          limit: 2000, // 2kb
+          limit: 2000 // 2kb
         }
       },
       {
@@ -36,7 +68,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       }
-    ],
+    ]
   },
   plugins: [
     new webpack.BannerPlugin({
@@ -48,21 +80,27 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       TWO: JSON.stringify('1+1'),
-      'api.domain': JSON.stringify('http://dev.api.domain.com'),
+      'api.domain': JSON.stringify('http://dev.api.domain.com')
     }),
     new HtmlWebpackPlugin({
       template: './index.html',
       templateParameters: {
-        env: process.env.NODE_ENV === 'development' ? '(개발용)' : '',
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
       },
-      minify: process.env.NODE_ENV === 'production' ? {
-        collapseWhitespace: true,
-        removeComments: true,
-      } : false,
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true
+            }
+          : false
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: '[name].css?[hash]'
     }),
+    new CopyPlugin([
+      { from: './node_modules/axios/dist/axios.min.js', to: './axios.min.js' }
+    ])
   ]
 };
